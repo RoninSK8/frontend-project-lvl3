58299c5c5c4933c19bffd28bff718e3aa7a92395
Сергей Политаев <ronin@sonikelf.ru>
2021-04-28 07:51:27 +0300

    added i18n initialization and refactored errors catching process

--- src/app.js
+++ src/app.js
@@ -6,101 +6,114 @@ import parse from './parser.js';
 import watch from './view.js';
 import resources from './locales';
 
-const schema = yup.object().shape({
-  input: yup.string().url(),
-});
-
-const validate = (fields) => {
-  try {
-    schema.validateSync(fields, { abortEarly: false });
-    return {};
-  } catch (e) {
-    return e.message;
-  }
-};
-
 export default () => {
-  const state = {
-    form: {
-      processState: 'filling',
-      processError: null,
-      field: {
-        input: '',
-      },
-      valid: true,
-      error: '',
-    },
-    feeds: [],
-    posts: [],
-  };
+  const i18nInstance = i18n.createInstance();
+  i18nInstance.init({
+    lng: 'ru',
+    debug: true,
+    fallbackLng: ['ru'],
+    resources,
+  })
+    .then(() => {
+      yup.setLocale({
+        string: {
+          required: i18nInstance.t('feedback.fieldRequired'),
+          url: i18nInstance.t('feedback.urlNotValid'),
+        },
+      });
 
-  const form = document.querySelector('form');
-  // const submitButton = form.querySelector('[type="submit"]');
-  // const formField = form.querySelector('.form-control');
-  // const feedback = document.querySelector('.feedback');
+      const schema = yup.object().shape({
+        input: yup.string().required().url(),
+      });
 
-  const watchedState = watch(state);
+      const validate = (fields) => {
+        try {
+          schema.validateSync(fields, { abortEarly: false });
+          return {};
+        } catch (e) {
+          return e.message;
+        }
+      };
+      const state = {
+        form: {
+          processState: 'filling',
+          processError: null,
+          field: {
+            input: '',
+          },
+          valid: '',
+          error: '',
+        },
+        feeds: [],
+        posts: [],
+      };
 
-  const updateValidationState = () => {
-    const feedLinks = state.feeds.map((feed) => feed.link);
-    if (_.includes(feedLinks, watchedState.form.field.input)) {
-      watchedState.form.valid = false;
-      watchedState.form.error = 'RSS уже существует';
-      return;
-    }
-    const errors = validate(watchedState.form.field);
-    watchedState.form.valid = _.isEqual(errors, {});
-    watchedState.form.error = errors;
-  };
+      const form = document.querySelector('form');
+      // const submitButton = form.querySelector('[type="submit"]');
+      // const formField = form.querySelector('.form-control');
+      // const feedback = document.querySelector('.feedback');
 
-  form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const input = formData.get('url');
-    watchedState.form.field.input = input;
+      const watchedState = watch(state, i18nInstance);
 
-    updateValidationState();
-
-    if (watchedState.form.valid) {
-      const url = new URL(input);
-      const proxyUrl = `https://hexlet-allorigins.herokuapp.com/get?&url=${encodeURIComponent(url)}`;
-      axios.get(proxyUrl)
-        .then((response) => {
-          const content = response.data.contents;
-          // const parser = new DOMParser();
-          // const xml = parser.parseFromString(contents, 'application/xml');
-          const feedData = parse(content);
-
-          console.log(feedData);
-
-          const feedLink = watchedState.form.field.input;
-          const feedTitle = feedData.feed.title;
-          const feedDescription = feedData.feed.description;
-          const newFeed = {
-            link: feedLink,
-            title: feedTitle,
-            description: feedDescription,
-          };
-          watchedState.feeds = [newFeed].concat(watchedState.feeds);
-
-          const newPosts = feedData.feed.posts;
-          watchedState.posts = newPosts.concat(watchedState.posts);
-
-          console.log(state);
-          // watchedState.feeds.push(input);
-          // displayFeed(xml);
-        })
-        .catch((err) => {
-          watchedState.form.error = err.message;
+      const updateValidationState = () => {
+        const feedLinks = state.feeds.map((feed) => feed.link);
+        if (_.includes(feedLinks, watchedState.form.field.input)) {
           watchedState.form.valid = false;
-          console.log(watchedState.form.error);
-        });
+          watchedState.form.error = i18nInstance.t('feedback.alreadyExists');
+        } else {
+          const errors = validate(watchedState.form.field);
+          watchedState.form.valid = _.isEqual(errors, {});
+          watchedState.form.error = errors;
+        }
+      };
 
-      // const feedData = parseXml(xml);
-      // watchedState.feed = feedData;
-    }
-    // console.log(state);
-    // watchedState.form.errors = errors;
-    // watchedState.form.processState = 'sending';
-  });
+      form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const input = formData.get('url');
+        watchedState.form.field.input = input;
+
+        updateValidationState();
+
+        if (watchedState.form.valid) {
+          const url = new URL(input);
+          const proxyUrl = `https://hexlet-allorigins.herokuapp.com/get?&url=${encodeURIComponent(url)}`;
+          axios.get(proxyUrl)
+            .then((response) => {
+              const content = response.data.contents;
+              const feedData = parse(content);
+
+              console.log(feedData);
+
+              const feedLink = watchedState.form.field.input;
+              const feedTitle = feedData.feed.title;
+              const feedDescription = feedData.feed.description;
+              const newFeed = {
+                link: feedLink,
+                title: feedTitle,
+                description: feedDescription,
+              };
+              watchedState.feeds = [newFeed].concat(watchedState.feeds);
+
+              const newPosts = feedData.feed.posts;
+              watchedState.posts = newPosts.concat(watchedState.posts);
+
+              console.log(state);
+              // watchedState.feeds.push(input);
+              // displayFeed(xml);
+            })
+            .catch((err) => {
+              watchedState.form.error = err.message;
+              watchedState.form.valid = false;
+              console.log(watchedState.form.error);
+            });
+
+          // const feedData = parseXml(xml);
+          // watchedState.feed = feedData;
+        }
+        // console.log(state);
+        // watchedState.form.errors = errors;
+        // watchedState.form.processState = 'sending';
+      });
+    });
 };
