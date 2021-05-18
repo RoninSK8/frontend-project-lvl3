f79b7afb105df661eb52e06e60661d31554a5be9
Сергей Политаев <ronin@sonikelf.ru>
2021-05-19 02:30:06 +0300

    fixing network error catch

--- src/app.js
+++ src/app.js
@@ -132,10 +132,10 @@ export default (i18n) => {
               watchedState.form.error = 'feedback.networkError';
               break;
             case 'Request failed with status code 500':
-              watchedState.form.error = 'feedback.rssParsingError';
+              watchedState.form.error = 'feedback.networkError';
               break;
             default:
-              watchedState.form.error = 'feedback.networkError';
+              watchedState.form.error = 'feedback.rssParsingError';
               break;
           }
           watchedState.form.processState = 'failed';
