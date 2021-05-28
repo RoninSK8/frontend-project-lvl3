060742bc8f4cda83d50e0529c62518989ecd066e
Сергей Политаев <ronin@sonikelf.ru>
2021-05-28 05:21:51 +0300

    added returning promise in init.js

--- src/init.js
+++ src/init.js
@@ -4,11 +4,12 @@ import resources from './locales';
 
 export default () => {
   const i18nInstance = i18n.createInstance();
-  i18nInstance.init({
+  const promise = i18nInstance.init({
     lng: 'ru',
     debug: false,
     fallbackLng: ['ru'],
     resources,
   })
     .then(() => app(i18nInstance));
+  return promise;
 };
